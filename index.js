c272c8765923c355a3f55c337c105ef8834421b1
CarlEverest78 <thepythoneer@gmail.com>
2022-06-16 00:48:46 +0200

    faster loading

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const searchForm = document.getElementById("search-form")
 const mainContainer = document.getElementById("default-message")
 const searchInput = document.getElementById("search")
 
-function renderHtml(array) {
-    mainContainer.innerHTML = ""
-    array.map(movie => {
-        mainContainer.innerHTML += 
-        `
-        <div class="movie-wrapper">
-            <img class="movie-img" src="${movie.Poster}" alt="Movie poster of ${movie.Title}">
-            <div class="movie-data">
-                <div class="movie-data-row"><h2>${movie.Title}</h2> <i class="fa-solid fa-star" style="color:#FEC654"></i> <p>${movie.Rating}</p></div>
-                <div class="movie-data-row"><p>${movie.Runtime}</p> <p>${movie.Genre}</p> <i class="fa-solid fa-circle-plus"></i> <p>Watchlist</p></div>
-                <div class="movie-data-row plot"><p>${movie.Plot}</p></div>
-            </div>
+function renderHtml(movie) {
+    searchInput.value = ""
+    searchInput.placeholder = "\uf002  Search for another movie"
+    
+    mainContainer.innerHTML += 
+    `
+    <div class="movie-wrapper">
+        <img class="movie-img" src="${movie.Poster}" alt="Movie poster of ${movie.Title}">
+        <div class="movie-data">
+            <div class="movie-data-row"><h2>${movie.Title}</h2> <i class="fa-solid fa-star" style="color:#FEC654"></i> <p>${movie.Rating}</p></div>
+            <div class="movie-data-row"><p>${movie.Runtime}</p> <p>${movie.Genre}</p> <i class="fa-solid fa-circle-plus"></i> <p>Watchlist</p></div>
+            <div class="movie-data-row plot"><p>${movie.Plot}</p></div>
         </div>
-        `
-    })
+    </div>
+    `
 }
 
 function renderError() {
@@ -38,6 +38,7 @@ async function searchMovies(value) {
 async function getMovies(value) {
     let array = await searchMovies(value)
     if (array !== undefined) {
+        mainContainer.innerHTML = ""
         for (let movie of array) {
             let response2 = await fetch(`https://www.omdbapi.com/?apikey=24705dce&i=${movie.imdbID}`)
             let data2 = await response2.json()
@@ -46,8 +47,9 @@ async function getMovies(value) {
             movie.Genre = data2.Genre
             movie.Plot = data2.Plot;
             (data2.Poster === "N/A") && (movie.Poster = "images/no-image-available.jpg")
+            console.log(movie)
+            renderHtml(movie)
         }
-        renderHtml(array)
     } else {
         renderError()
     }
@@ -58,8 +60,6 @@ searchForm.addEventListener("submit", function(event) {
     event.preventDefault()
     const formData = new FormData(event.target)
     let search = formData.get("search")
-    searchInput.value = ""
-    searchInput.placeholder = "\uf002  Search for another movie"
     getMovies(search)
 })
 
