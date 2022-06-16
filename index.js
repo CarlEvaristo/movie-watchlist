7e6010f77774f55f1e8b8016a8e8785f2358835e
CarlEverest78 <thepythoneer@gmail.com>
2022-06-16 09:19:42 +0200

    first commit: debug omdb http issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const searchInput = document.getElementById("search")
 function renderHtml(movie) {
     searchInput.value = ""
     searchInput.placeholder = "\uf002  Search for another movie"
-    
+
     mainContainer.innerHTML += 
     `
     <div class="movie-wrapper">
@@ -24,23 +24,26 @@ function renderError() {
     searchInput.placeholder = "\uf002 No Data" 
     mainContainer.innerHTML = 
     `   <div id="default-message" class="container">
-            <h3>Unable to find what you’re looking for.<br> Please try another search.</h3>
+            <h3>Unable to find what you're looking for.<br> Please try another search.</h3>
         </div>
     `
 }
 
-async function searchMovies(value) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=24705dce&s=${value}`)
+async function searchMovies(query) {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=d5f56738&s=${query}`)
     let movieArr = await response.json()
+    console.log(movieArr)
     return movieArr.Search
 }
 
-async function getMovies(value) {
-    let array = await searchMovies(value)
-    if (array !== undefined) {
-        mainContainer.innerHTML = ""
+async function getMovies(query) {
+    let array = await searchMovies(query)
+    console.log("Array", array)
+    mainContainer.innerHTML = ""
+
+    try {
         for (let movie of array) {
-            let response2 = await fetch(`https://www.omdbapi.com/?apikey=24705dce&i=${movie.imdbID}`)
+            let response2 = await fetch(`http://www.omdbapi.com/?apikey=d5f56738&i=${movie.imdbID}`)
             let data2 = await response2.json()
             movie.Rating = data2.imdbRating
             movie.Runtime = data2.Runtime
@@ -50,7 +53,9 @@ async function getMovies(value) {
             console.log(movie)
             renderHtml(movie)
         }
-    } else {
+    } catch(error) {
+        console.log("d5f56738")
+        console.log(error)
         renderError()
     }
 }
